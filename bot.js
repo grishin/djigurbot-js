ab748ab5ea390e67aad05c5faaf95764a408789a
Андрей Гришин <andrej.grishin@gmail.com>
2016-03-15 08:54:57 +0000

    bot goodnights

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 var toasts = require("./toasts.js");
+var nights = require("./good-nights.js")
 
 var TelegramBot = require('node-telegram-bot-api');
 var bot = new TelegramBot(process.env.DJIGURBOT_TELEGRAMAPIKEY, {polling: true});
@@ -15,7 +16,7 @@ bot.onText(/твоя!/i, (msg, match) => {
   bot.sendMessage(msg.chat.id, "Нееееет, твоя!");
 });
 
-bot.onText(/крутой/i, (msg, match) => {
+bot.onText(/крутой/i, (msg, match) => {   
   bot.sendMessage(msg.chat.id, `Нееееет, ${msg.from.first_name}, это ты крутой!`);
 });
 
@@ -23,8 +24,18 @@ bot.onText(/сокиабле/i, (msg, match) => {
   bot.sendMessage(msg.chat.id, "Сокиабле? ЧОБЛЯ?");
 });
 
+// ЙОХОХО! И БУТЫЛКА РОМУ!
+
 bot.onText(/доброе утро/i, (msg, match) => {
   bot.sendMessage(msg.chat.id, `И тебе наидобрейшего утра, ${msg.from.first_name}!`);
+});
+
+bot.onText(/спокойной ночи/i, (msg, match) => {
+  bot.sendMessage(msg.chat.id, `${msg.from.first_name}, ${nights.getRandomNight()}`);
+});
+
+bot.onText(/тест!/i, (msg, match) => {
+  bot.sendMessage(msg.chat.id, `Я тут, ${msg.from.first_name} !`);
 });
 
 bot.onText(/поздравляй!/i, function (msg, match) {
@@ -46,6 +57,18 @@ bot.onText(/Тост!/i, function (msg, match) {
   bot.sendMessage(msg.chat.id, "Опять бухать!?!? Работать кто будет?");
 });
 
+bot.onText(/sudo Тост!/i, function (msg, match) {
+  bot.sendMessage(msg.chat.id, toasts.getRandomToast());
+});
+
 bot.onText(/Тост говори!/i, function (msg, match) {
   bot.sendMessage(msg.chat.id, toasts.getRandomToast());
+});
+
+bot.onText(/чатик/i, function (msg, match) {
+  bot.sendMessage(msg.chat.id, "Я не хочу в отдельный чатик, Вадим!!!");
+});
+
+bot.onText(/плохо/i, function (msg, match) {
+  bot.sendMessage(msg.chat.id, `Хочешь поговорить об этом, ${msg.from.first_name}?`);
 });
